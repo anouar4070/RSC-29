488e9006d2eb46b9a9cba5d918cf1dc79841d457
anouar garbaa <g.anouar@yahoo.com>
2024-12-12 10:37:19 +0100

    Fetching Data with RSC

--- app/page.js
+++ app/page.js
@@ -1,7 +1,24 @@
+import DataFetchingDemo from "@/components/DataFetchingDemo";
+
+
 export default function Home() {
   return (
     <main>
-      <p>Let's go!</p>
+      <DataFetchingDemo />
     </main>
   );
 }
+
+
+// import ClientDemo from "@/components/ClientDemo";
+// import RSCDemo from "@/components/RSCDemo";
+
+// export default function Home() {
+//   return (
+//     <main>
+//       <ClientDemo>
+//         <RSCDemo />
+//       </ClientDemo>
+//     </main>
+//   );
+// }
